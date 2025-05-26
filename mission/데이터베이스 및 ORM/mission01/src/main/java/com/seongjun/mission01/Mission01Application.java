f0de354f5f0e4210588d53cc5fe63d12f3262cbb
SeongJun98 <seongjunin98@gmail.com>
2025-05-26 21:27:03 +0900

    mission : Database and ORM

---
+++ mission/데이터베이스 및 ORM/mission01/src/main/java/com/seongjun/mission01/Mission01Application.java
@@ -0,0 +1,13 @@
+package com.seongjun.mission01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mission01Application.class, args);
+    }
+
+}
